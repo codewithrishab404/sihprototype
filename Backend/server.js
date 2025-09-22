e11ae57bacc2a7d3358306c98586c5961e86016a
rishab <rishabmallick20004@gmail.com>
2025-09-22 20:28:38 +0530

    database is successfully connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 // Packages
 import express from "express";
 import dotenv from 'dotenv';
-
+import connectDB from "./config/db.js";
 dotenv.config();
 const port = process.env.PORT || 8000;
-
+connectDB();
 const app = express();
 app.use(express.json()); // Express dont understand json , so it will convert json data sent by user into JS  object
 app.use(express.urlencoded({
