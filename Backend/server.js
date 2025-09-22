6205cdadfe0f02ce589c59bc04ae0e0efafe708f
rishab <rishabmallick20004@gmail.com>
2025-09-22 20:02:32 +0530

    Stop tracking node_modules

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+// Packages
+import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+app.use(express.json()); // Express dont understand json , so it will convert json data sent by user into JS  object
+app.use(express.urlencoded({
+    extended: true,
+    parameterLimit: 5000,   // parameterLimit will prevent Dos Attack
+}));
+app.listen(port, () => console.log(`Server is running on : ${port}`));
